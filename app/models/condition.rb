cf30ddc85f276aa5479c866272ec84db7cacdd35
heislercreative <brennan@heislercreative.com>
2018-03-25 00:09:34 -0600

    create condition list

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,3 +1,22 @@
 class Condition < ActiveRecord::Base
   belongs_to :patient
+
+  def self.list
+    @@condition_list
+  end
+
+  @@condition_list = {
+    "acne",
+    "actinic keratosis",
+    "atypical mole",
+    "basal cell carcinoma",
+    "contact dermatitis",
+    "cyst",
+    "dermatitis",
+    "eczema",
+    "lipoma",
+    "melanoma",
+    "squamous cell carcinoma",
+    "wart(s)"
+  }
 end
