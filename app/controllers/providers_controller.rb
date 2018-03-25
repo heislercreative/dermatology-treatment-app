ea05e363cbcb471b2d48ba7b0aa93040c1ca80d5
heislercreative <brennan@heislercreative.com>
2018-03-24 18:16:04 -0600

    added signup post route

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -4,6 +4,15 @@ class ProvidersController < ApplicationController
     erb :signup
   end
 
+  post '/signup' do
+    provider = Provider.new(params)
+    if provider.save
+      redirect to '/login'
+    else
+      redirect to '/failure'
+    end
+  end
+
   get '/home' do
     erb :'/providers/home'
   end
